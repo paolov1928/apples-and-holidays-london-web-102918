ef37f8ce9cb1c4c9b81f901ef1f70225a1fe8c22
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-15 14:56:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash
 
+holiday_hash.each do |k,v|
+  puts k
+  puts v
+end
 
 end
 
